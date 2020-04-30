71f8aa9d0827ccdfe7f5bc054824b1f5d089bd5b
PatrickKR <mailpatrickkorea@gmail.com>
2020-04-30 23:58:37 +0900

    Removed unnecessary dependency (ProtocolLib) Removed unnecessary task (javadoc)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ version = properties["pluginVersion"]!!
 repositories {
     maven("https://repo.maven.apache.org/maven2/")
     maven("https://papermc.io/repo/repository/maven-public/")
-    maven("https://repo.dmulloy2.net/nexus/repository/public/")
     maven("https://maven.enginehub.org/repo/")
     maven("https://jitpack.io/")
 }
@@ -16,7 +15,6 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.destroystokyo.paper:paper-api:1.15.2-R0.1-SNAPSHOT")
-    implementation("com.comphenix.protocol:ProtocolLib:4.5.0")
     implementation("com.sk89q.worldedit:worldedit-bukkit:7.1.0")
     implementation("com.github.noonmaru:tap:2.3.1")
 }
@@ -24,10 +22,6 @@ dependencies {
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
-    }
-
-    javadoc {
-        options.encoding = "UTF-8"
     }
 
     processResources {
