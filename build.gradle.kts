a6dca8d6242de4056e6a850892cb1ef3e108d149
devil0414 <73186833+devil0414@users.noreply.github.com>
2020-12-10 00:42:16 +0900

    1.16.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,10 @@ repositories {
 dependencies {
     compileOnly(kotlin("stdlib-jdk8")) //kotlin
     compileOnly("junit:junit:4.12") //junit
-    compileOnly("com.destroystokyo.paper:paper-api:1.16.1-R0.1-SNAPSHOT") //paper
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT") //paper
     compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0-SNAPSHOT") //protocollib
-    compileOnly("com.github.noonmaru:tap:2.8.8") //tap
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.1.0") //worldedit
+    compileOnly("com.github.noonmaru:tap:3.2.6") //tap
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.0") //worldedit
     implementation("com.github.noonmaru:kommand:0.1.9")
 }
 
